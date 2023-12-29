d62358069ad86804b8511cba44766a6df7b3b8b0
Fabio Privitera <fabio_privitera@yahoo.it>
2023-12-29 11:37:24 +0200

    base project

---
+++ app.js
@@ -0,0 +1,26 @@
+//I did not install any dependencies
+//node.js ships with http so it will work out of the box
+const path=require('path')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require("./routes/shop")
+
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>PAGE NOT FOUND</h1>')
+})
+
+
+
+
+
+app.listen(3000)
---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express')
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log('in the middleware at path /');
+
+  res.sendFile(path.join(rootDir, 'views','shop.html'));
+});
+
+module.exports = router
